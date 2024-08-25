01ac5f8802d53db590285ec0ffc1fed8fb0372cf
nancysagar <nancysagat4726@gmail.com>
2024-08-25 14:04:38 +0530

    render deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file
+import SalesGrowthOverTime from './components/SalesGrowthOverTime';
+import SalesOverTime from './components/SalesOverTime';
+import CLVByCohorts from './components/CLVByCohorts';
+import NewCustomersOverTime from './components/NewCustomersOverTime';
+import CustomerGeography from './components/CustomerGeography';
+import RepeatCustomers from './components/RepeatCustomers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Visualization Dashboard</h1>
+      <div className="chart-container">
+        <SalesOverTime />
+      </div>
+      <div className="chart-container">
+        <SalesGrowthOverTime />
+      </div>
+      <div className="chart-container">
+        <NewCustomersOverTime />
+      </div>
+      <div className="chart-container">
+        <RepeatCustomers />
+      </div>
+      <div className="chart-container">
+        <CustomerGeography />
+      </div>
+      <div className="chart-container">
+        <CLVByCohorts />
+      </div>
     </div>
   );
 }
